56aea1fde3630e6b7c4e682e04b9f2ac6528c794
nubiaZ <nubiaxz@icloud.com>
2025-02-27 12:33:53 -0600

    google-java-format commented out

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,18 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "New-RobotCode-2024-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "9d23bd13d7245d90573ad99d12b343584ee67db2";
-  public static final String GIT_DATE = "2025-02-26 19:33:39 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "e360b4823a5a05a18080f050baa687ff59b9cb56";
+  public static final String GIT_DATE = "2025-02-26 19:56:19 EST";
   public static final String GIT_BRANCH = "Coral";
-  public static final String BUILD_DATE = "2025-02-26 19:41:59 EST";
-  public static final long BUILD_UNIX_TIME = 1740616919926L;
+  public static final String BUILD_DATE = "2025-02-27 13:31:35 EST";
+  public static final long BUILD_UNIX_TIME = 1740681095744L;
   public static final int DIRTY = 0;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
-
