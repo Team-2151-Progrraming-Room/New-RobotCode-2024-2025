9d23bd13d7245d90573ad99d12b343584ee67db2
Daniel525183 <ps525183@pths209.org>
2025-02-26 18:33:39 -0600

    New repository

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "New RobotCode 2024-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "7b806161b3c43d0495df3f7c2103c70050a48655";
+  public static final String GIT_DATE = "2025-02-26 19:20:36 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-02-26 19:30:37 EST";
+  public static final long BUILD_UNIX_TIME = 1740616237281L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
