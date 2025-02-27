a3de3c16267623ea354ff8a0d6049a9cebaac16e
Daniel525183 <ps525183@pths209.org>
2025-02-27 12:43:21 -0600

    New buildConstants after I recloned the repository (had to do so because remote branches were not showing up)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "New RobotCode 2024-2025";
+  public static final String MAVEN_NAME = "New-RobotCode-2024-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "9d23bd13d7245d90573ad99d12b343584ee67db2";
-  public static final String GIT_DATE = "2025-02-26 19:33:39 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "eee5048240eea78873aa4db6e3dfe1223cace4ef";
+  public static final String GIT_DATE = "2025-02-27 13:25:24 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-27 13:22:33 EST";
-  public static final long BUILD_UNIX_TIME = 1740680553369L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-27 13:41:41 EST";
+  public static final long BUILD_UNIX_TIME = 1740681701828L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
