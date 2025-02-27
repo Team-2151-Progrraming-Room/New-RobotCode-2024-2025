1f61e226c4d9a52c6b4d9f4ec61121b997b0bcd8
Risk-joke <yeetyamcgeet@gmail.com>
2025-02-26 19:01:00 -0600

    Added Algae Stuff to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,15 +21,19 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.GyroIO;
-import frc.robot.subsystems.drive.GyroIOPigeon2;
-import frc.robot.subsystems.drive.ModuleIO;
-import frc.robot.subsystems.drive.ModuleIOSim;
-import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import java.util.function.BooleanSupplier;
+
+//our subsystems
+import frc.robot.subsystems.ArmSubsystemCTRE;
+import frc.robot.subsystems.AlgaeSubsystemCTRE;
+
+//out commands
+import frc.robot.commands.AlgaeShooterCommands;
+
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -40,70 +44,42 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
   // Subsystems
-  private final Drive drive;
+  private final ArmSubsystemCTRE arm = new ArmSubsystemCTRE();;
+  private final AlgaeSubsystemCTRE algae = new AlgaeSubsystemCTRE();;
 
+  //boolean supplier
+  BooleanSupplier m_dynamicAtShootSpeed = () -> algae.atShooterSpeed();;
+
+  //commands
+  private final Command m_algaeShootCommand = new AlgaeShooterCommands(algae, m_dynamicAtShootSpeed).getShootCommand();
+  private final Command m_algaeDumpCommand = new AlgaeShooterCommands(algae, m_dynamicAtShootSpeed).getDumpCommand();
+  private final Command m_algaeIntakeCommand = new AlgaeShooterCommands(algae, m_dynamicAtShootSpeed).getIntakeCommand();
+  private final Command m_algaeDepositCommand = new AlgaeShooterCommands(algae, m_dynamicAtShootSpeed).getDepositCommand();
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
+  private final Joystick buttonBoard = new Joystick(1);
+
+  public final JoystickButton button1;
+  public final JoystickButton button2;
+  public final JoystickButton button3;
+  public final JoystickButton button4;
+  public final JoystickButton button5;
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
-        break;
+    button1 = new JoystickButton(buttonBoard, 1);
+    button2 = new JoystickButton(buttonBoard, 2);
+    button3 = new JoystickButton(buttonBoard, 3);
+    button4 = new JoystickButton(buttonBoard, 4);
+    button5 = new JoystickButton(buttonBoard, 5);
 
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
-        break;
-
-      default:
-        // Replayed robot, disable IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-        break;
-    }
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-
-    // Set up SysId routines
-    autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-
+  
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -115,39 +91,12 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    // Default command, normal field-relative drive
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> -controller.getLeftY(),
-            () -> -controller.getLeftX(),
-            () -> -controller.getRightX()));
+    button2.onTrue(m_algaeShootCommand);
+    button3.onTrue(m_algaeDumpCommand);
+    button4.onTrue(m_algaeIntakeCommand);
+    button5.onTrue(m_algaeDepositCommand);
 
-    // Lock to 0° when A button is held
-    controller
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY(),
-                () -> -controller.getLeftX(),
-                () -> new Rotation2d()));
-
-    // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
-
-    // Reset gyro to 0° when B button is pressed
-    controller
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
-                .ignoringDisable(true));
   }
-
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
