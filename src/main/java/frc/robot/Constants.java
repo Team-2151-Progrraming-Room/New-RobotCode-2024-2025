fb6b42ccc69db718cbca360c002efce1fa1ddcea
Risk-joke <yeetyamcgeet@gmail.com>
2025-02-26 18:47:42 -0600

    Moved Branch to new Folder

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,4 +34,101 @@ public final class Constants {
     /** Replaying from a log file. */
     REPLAY
   }
+  public static final class DriveTrainConstants{
+    public static final int Pigeon = 10;
+
+    public static final int FRMotor1 = 1;
+    public static final int FRMotor2 = 2;
+    public static final int FRCanCoder = 3;
+
+    public static final int FLMotor1 = 4;
+    public static final int FLMotor2 = 5;
+    public static final int FLCanCoder = 6;
+
+    public static final int BRMotor1 = 11;
+    public static final int BRMotor2 = 12;
+    public static final int BRCanCoder = 13;
+
+    public static final int BLMotor1 = 14;
+    public static final int BLMotor2 = 15;
+    public static final int BLCanCoder = 16;
+
+  }
+
+  public static final class ArmConstants{
+    public static final int kArmMotor = 20;
+    public static final int kArmMotor2 = 21;
+    public static final int kLockMotor = 22;
+    public static final int kArmCANcoder = 23;
+
+    public static final double kArmSpeedUp = 0.5;
+    public static final double kArmSpeedDown = -0.5;
+
+    public static final int kArmMotorCurrentLimit = 40;
+    public static final int kLockMotorCurrentLimit = 40;
+
+    public static final int kArmPIDControllerP = 0;
+    public static final int kArmPIDControllerI = 0;
+    public static final int kArmPIDControllerD = 0;
+    public static final int kArmPIDControllerFF = 0;
+    public static final int kArmPIDControllerIZone = 0;
+    public static final int kArmPIDControllerOutputRange = 5;
+    public static final int kArmPIDControllerOutputMin = 5;
+    public static final int kArmPIDControllerOutputMax = 5;
+
+    public static final double kArmCANCoderConversionFactor = 45.5111111;
+
+    //arm positions in degrees 
+    public static final int kArmPositionGroundAlgae = 110;
+    public static final int kArmPositionLowAlgae = 15;
+    public static final int kArmPositionProcessor = 110;
+    public static final int kArmPositionHighAlgae = 45;
+    public static final int kPositionShoot = 110;
+    public static final int kArmPositionClimb = 110;
+    
+  }
+  
+  public static final class AlgaeConstants {
+
+    public static final int kAlgaeRevMotorID = 31;
+    public static final int kAlgaeRev2MotorID = 32;
+    public static final int kAlgaeKickMotorID = 33;
+
+    public static final int kAlgaeRevMotorCurrentLimit = 40;
+    public static final int kAlgaeRev2MotorCurrentLimit = 40;
+    public static final int kAlgaeKickMotorCurrentLimit = 40;
+
+    //PID Constants
+    public static final int kAlgaePIDControllerS = 0;
+    public static final int kAlgaePIDControllerV = 0;
+    public static final int kAlgaePIDControllerP = 0;
+    public static final int kAlgaePIDControllerI = 0;
+    public static final int kAlgaePIDControllerD = 0;
+    public static final int kAlgaeFeed = 0;//force to overcome gravity
+    
+
+    public static final int kAlgaeEncoderConversionFactor = 1000;//used to convert rpm to velocity
+
+    public static final int kAlgaeRevVelocity = 10;
+    public static final int kAlgaeRev2Velocity = 10;
+
+    public static final int kAlgaeSpeedTolerance = 2;
+    
+    //actions that don't require pid control
+    public static final double kAlgaeKickMotorON = .75;
+    public static final double kAlgaeIntake = 0.5;
+    
+    public static final int kAlgaeVoltage = 0;
+
+    public static final double kShortShooterWaitTime = 0.25;
+    public static final double kLongShooterWaitTime = 0.5;
+  }
+
+  public static class CanbusName{
+    public static final String armCANBus = "rio";
+  }
+
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystemCTRE.java
@@ -0,0 +1,138 @@
+// Imports
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.MathUtil;
+
+import static edu.wpi.first.units.Units.*;
+
+import java.io.ObjectInputFilter.Config;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.util.sendable.SendableBuilder;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.hardware.TalonFXS;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+
+import frc.robot.Robot;
+import frc.robot.Constants.AlgaeConstants;
+
+// Main Class
+public class AlgaeSubsystemCTRE extends SubsystemBase{
+    TalonFXS m_Rev = new TalonFXS(AlgaeConstants.kAlgaeRevMotorID);
+    TalonFXS m_Rev2  = new TalonFXS(AlgaeConstants.kAlgaeRev2MotorID);
+    TalonFXS m_Kick = new TalonFXS(AlgaeConstants.kAlgaeKickMotorID);
+
+    TalonFXSConfiguration configs = new TalonFXSConfiguration();
+    
+    Slot0Configs slot0;
+    
+    // Sub Class
+    public AlgaeSubsystemCTRE (){
+        slot0 = configs.Slot0;
+        slot0.kS = AlgaeConstants.kAlgaePIDControllerS;
+        slot0.kV = AlgaeConstants.kAlgaePIDControllerV;
+        slot0.kP = AlgaeConstants.kAlgaePIDControllerP;
+        slot0.kI = AlgaeConstants.kAlgaePIDControllerI;
+        slot0.kD = AlgaeConstants.kAlgaePIDControllerD;
+
+
+        m_Rev.getConfigurator().apply(configs);
+        m_Rev.setInverted(true);
+        m_Rev2.getConfigurator().apply(configs);
+
+        m_request = new VelocityVoltage(AlgaeConstants.kAlgaeVoltage).withSlot(0);
+    }
+
+    final VelocityVoltage m_request;
+
+    public void algaeIntake(){
+        m_Rev.set(AlgaeConstants.kAlgaeIntake);
+        m_Rev2.set(AlgaeConstants.kAlgaeIntake);
+        
+    }
+
+    // For the Motors to turn OFF!
+    public void RevMotorsOFF(){
+        m_Rev.stopMotor();
+        m_Rev2.stopMotor();
+    }
+
+    public void KickMotorOFF(){
+        m_Kick.stopMotor();
+    }
+
+    public void allMotorsOFF(){
+        m_Rev.stopMotor();
+        m_Rev2.stopMotor();
+        m_Kick.stopMotor();
+    }
+
+    // For the Motors to turn ON!
+    public void RevMotorsSHOOT(){
+        m_Rev.setControl(m_request.withVelocity(AlgaeConstants.kAlgaeRevVelocity).withFeedForward(AlgaeConstants.kAlgaeFeed));
+        m_Rev2.setControl(m_request.withVelocity(AlgaeConstants.kAlgaeRev2Velocity).withFeedForward(AlgaeConstants.kAlgaeFeed));
+    }
+    
+    public void KickMotorON(){
+        m_Kick.set(AlgaeConstants.kAlgaeKickMotorON);
+    }
+
+    public double getRevVelocity(){
+        double velocity = m_Rev.getVelocity().getValueAsDouble();
+        System.out.println(velocity);
+        return velocity;
+    }
+
+    public double getRev2Velocity(){
+        double velocity = m_Rev2.getVelocity().getValueAsDouble();
+        System.out.println(velocity);
+        return velocity;
+    }
+
+    public boolean atShooterSpeed() {
+
+        if (MathUtil.isNear(AlgaeConstants.kAlgaeRevVelocity, getRevVelocity(), AlgaeConstants.kAlgaeSpeedTolerance) && MathUtil.isNear(AlgaeConstants.kAlgaeRev2Velocity, getRev2Velocity(), AlgaeConstants.kAlgaeSpeedTolerance)) {
+          return true;
+        }
+        return false;
+      }
+
+    //Commands
+    
+    public Command RevMotorOFFCommand(){
+        return runOnce(
+            () -> {RevMotorsOFF();}
+        );
+    }
+
+    public Command KickMotorOFFCommand(){
+        return runOnce(
+            () -> {KickMotorOFF();}
+        );
+    }
+
+    public Command allMotorsOFFCommand(){
+        return runOnce(
+            () -> {allMotorsOFF();}
+        );
+    }
+
+    public Command RevMotorsSHOOTCommand(){
+        return runOnce(
+            () -> {RevMotorsSHOOT();}
+        );
+    }
+
+    public Command KickMotorONCommand(){
+        return runOnce(
+            () -> {KickMotorON();}
+        );
+    }
+}
