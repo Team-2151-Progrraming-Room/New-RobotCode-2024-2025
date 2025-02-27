8f216faecb4d1e86625fc8faa4b531fce39ca7c4
Daniel525183 <ps525183@pths209.org>
2025-02-27 13:38:27 -0600

    Build constants generated

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "New-RobotCode-2024-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "dd126353ae15001eef1556556f3dfdc5e9f22359";
-  public static final String GIT_DATE = "2025-02-27 14:28:58 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "a102dfaf5ea50474b65543c2ed3904c08b387ae0";
+  public static final String GIT_DATE = "2025-02-27 14:33:48 EST";
   public static final String GIT_BRANCH = "ArmSubsystem";
-  public static final String BUILD_DATE = "2025-02-27 14:29:41 EST";
-  public static final long BUILD_UNIX_TIME = 1740684581973L;
+  public static final String BUILD_DATE = "2025-02-27 14:34:54 EST";
+  public static final long BUILD_UNIX_TIME = 1740684894175L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
