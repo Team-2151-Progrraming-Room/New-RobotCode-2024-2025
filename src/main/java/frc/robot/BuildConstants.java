deb3f61f7215ae65f037abc2c1a291fe38616412
Daniel525183 <ps525183@pths209.org>
2025-02-27 12:46:09 -0600

    Another build constants after I deployed this branch

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "New-RobotCode-2024-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "eee5048240eea78873aa4db6e3dfe1223cace4ef";
-  public static final String GIT_DATE = "2025-02-27 13:25:24 EST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "a3de3c16267623ea354ff8a0d6049a9cebaac16e";
+  public static final String GIT_DATE = "2025-02-27 13:43:21 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-27 13:41:41 EST";
-  public static final long BUILD_UNIX_TIME = 1740681701828L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-02-27 13:44:26 EST";
+  public static final long BUILD_UNIX_TIME = 1740681866385L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
