16c712e3a74a5e88d47f1de32e37b883029ee7fb
nubiaZ <nubiaxz@icloud.com>
2025-03-11 17:11:03 -0500

    pathplanner added

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "New-RobotCode-2024-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "3433a5ce61b6db6b416a78f049e4fb95978f02e0";
-  public static final String GIT_DATE = "2025-03-11 17:13:05 EDT";
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "336086747d90ea49eff9bcf5dcfb38d092c9834e";
+  public static final String GIT_DATE = "2025-03-11 17:52:31 EDT";
   public static final String GIT_BRANCH = "ArmSubsystem+Algae";
-  public static final String BUILD_DATE = "2025-03-11 17:46:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1741729562681L;
+  public static final String BUILD_DATE = "2025-03-11 18:04:14 EDT";
+  public static final long BUILD_UNIX_TIME = 1741730654524L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
