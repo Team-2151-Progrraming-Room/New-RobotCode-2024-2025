56bba0aaf46e0fb996f3ec1b5f1e123f271c9add
Daniel525183 <ps525183@pths209.org>
2025-03-17 17:41:57 -0500

    Most recent callibration of the drivetrain. It's not perfect, as there is still a slight turn when it goes forwards and backwards, but it should be enough for the competition. Merging this to main.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "New-RobotCode-2024-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "3ac00f4cb12dbeab3110ec41222c75dbdcceac28";
-  public static final String GIT_DATE = "2025-03-05 16:58:14 EST";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "afd118a18f8c624b937abb524725a6d58e574989";
+  public static final String GIT_DATE = "2025-03-06 17:41:01 EST";
   public static final String GIT_BRANCH = "drivetrainBranch";
-  public static final String BUILD_DATE = "2025-03-05 19:43:57 EST";
-  public static final long BUILD_UNIX_TIME = 1741221837340L;
+  public static final String BUILD_DATE = "2025-03-17 17:50:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1742248216987L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
