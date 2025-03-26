457ea3152cdee07fe0ea8b93a1c8c54603f69b39
Daniel525183 <ps525183@pths209.org>
2025-03-26 16:46:16 -0500

    paths updated to get rid of wait time- user can input it instead

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "New-RobotCode-2024-2025-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 200;
-  public static final String GIT_SHA = "5b71d4175e92adc1b64a685f76b5faec6c31f3cc";
-  public static final String GIT_DATE = "2025-03-26 17:38:18 EDT";
+  public static final int GIT_REVISION = 201;
+  public static final String GIT_SHA = "5746b0cd2cc6d6547f8611758e0485be4d7a167c";
+  public static final String GIT_DATE = "2025-03-26 17:39:39 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-26 17:39:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1743025162761L;
+  public static final String BUILD_DATE = "2025-03-26 17:40:54 EDT";
+  public static final long BUILD_UNIX_TIME = 1743025254105L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
